ef7a96ca1434dad1c5456af72a8484af47c8e794
Edmond GAKS <edmondgaks@gmail.com>
2022-08-08 20:21:28 +0200

    adding the back tick to redirect to home page

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -3,7 +3,7 @@
 @include('partials._search')
 
 
-<a href="index.html" class="inline-block text-black ml-4 mb-4"
+<a href="/" class="inline-block text-black ml-4 mb-4"
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
