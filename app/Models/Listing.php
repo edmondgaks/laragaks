6b4c3a516fb719fc2c7d3f5b72103761280fda3c
Edmond GAKS <edmondgaks@gmail.com>
2022-08-08 16:33:45 +0200

    creating a model with artisan

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,30 +1,11 @@
 <?php
 
-Namespace App\Models;
-    
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id'=> '1',
-                'title' => 'Book 1',
-                'description' => 'Lorem Ipsum description of book 1',
-            ],
-            [
-                'id'=> '2',
-                'title' => 'Book 2',
-                'description' => 'Lorem Ipsum description of book 2'
-            ]
-        ];
-    }
-    public static function find($id) {
-        $listings = self::all();
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
-}
+namespace App\Models;
 
-?>
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
