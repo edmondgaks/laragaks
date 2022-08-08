41d5d63d99f1f4f8aead411c5f1ff5d47e063f8f
Edmond GAKS <edmondgaks@gmail.com>
2022-08-08 20:20:04 +0200

    updating the listing page

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
-# laragigs
+# laragaks
 
 
 <h2>Post your jobs </h2>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,11 +24,11 @@
                 },
             };
         </script>
-        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+        <title>LaraGaks | Find Laravel Jobs & Projects</title>
     </head>
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
-            <a href="index.html"
+            <a href="/"
                 ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
