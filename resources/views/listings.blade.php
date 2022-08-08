fe175b5290ac5a4b1735728681d87b927d570c9a
Edmond GAKS <edmondgaks@gmail.com>
2022-08-08 18:01:35 +0200

    fixing a small error of file handling

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -9,6 +9,7 @@
 <body>
     @extends('layout')
     @section('content')
+    @include('partials._hero')
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
         @unless(count($listings) == 0)
         @foreach ($listings as $listing)
@@ -17,11 +18,11 @@
                 <img
                     class="hidden w-48 mr-6 md:block"
                     src="images/skynet.png"
-                    alt=""
+                    alt="" 
                 />
                 <div>
                     <h3 class="text-2xl">
-                        <a href="show.html">{{$listing->title}}</a>
+                        <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
                     </h3>
                     <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                     <ul class="flex">
