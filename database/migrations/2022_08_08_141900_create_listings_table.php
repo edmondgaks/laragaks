bc84cab4ce6ac3aa363a886b52329d403172eef6
Edmond GAKS <edmondgaks@gmail.com>
2022-08-08 16:22:26 +0200

    adding different rows to our listing

--- database/migrations/2022_08_08_141900_create_listings_table.php
+++ database/migrations/2022_08_08_141900_create_listings_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('tags');
+            $table->string('company');
+            $table->string('location');
+            $table->string('email');
+            $table->string('website');
+            $table->longText('description');
             $table->timestamps();
         });
     }
