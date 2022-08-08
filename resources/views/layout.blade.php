0c7f9ebde89c62225dfad0e81bc91c8520f18215
Edmond GAKS <edmondgaks@gmail.com>
2022-08-08 17:56:35 +0200

    adding the html templates to be used in our project made also using taiwindcss

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,14 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laragigs</title>
-</head>
-<body>
-    <h1>Laragigs</h1>
-    {{-- VIEW OUTPUT--}}
-    @yield('content')
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="images/favicon.ico" />
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+            tailwind.config = {
+                theme: {
+                    extend: {
+                        colors: {
+                            laravel: "#ef3b2d",
+                        },
+                    },
+                },
+            };
+        </script>
+        <title>LaraGigs | Find Laravel Jobs & Projects</title>
+    </head>
+    <body class="mb-48">
+        <nav class="flex justify-between items-center mb-4">
+            <a href="index.html"
+                ><img class="w-24" src="images/logo.png" alt="" class="logo"
+            /></a>
+            <ul class="flex space-x-6 mr-6 text-lg">
+                <li>
+                    <a href="register.html" class="hover:text-laravel"
+                        ><i class="fa-solid fa-user-plus"></i> Register</a
+                    >
+                </li>
+                <li>
+                    <a href="login.html" class="hover:text-laravel"
+                        ><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                        Login</a
+                    >
+                </li>
+            </ul>
+        </nav>
+        {{-- VIEW OUTPUT--}}
+        
+            @yield('content')
+       
 </body>
 </html>
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,7 +1,9 @@
-
+@extends('layout')
+@section('content')
 <h2>
     {{$listing['title']}}
 </h2>
 <p>
     {{$listing['description']}}
 </p>
+@endsection
