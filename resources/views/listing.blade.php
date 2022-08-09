d827acd73333e036ed0c21411ca0dac545e6af48
Edmond GAKS <edmondgaks@gmail.com>
2022-08-09 10:04:32 +0200

    accessing class attributes outside the file by merging attributes

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,7 +7,7 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-<div class="bg-gray-50 border border-gray-200 p-10 rounded">
+<x-card class="p-24">
     <div
         class="flex flex-col items-center justify-center text-center"
     >
@@ -62,6 +62,6 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
 </div>
 @endsection
