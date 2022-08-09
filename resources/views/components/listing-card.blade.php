fe35fb4c8bcdfb66337cc2a12947d410d910788f
Edmond GAKS <edmondgaks@gmail.com>
2022-08-09 09:57:43 +0200

    adding the card file and using the variable slot

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,4 +1,6 @@
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+@props(['Listing'])
+
+<x-card>
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
@@ -11,19 +13,13 @@
             </h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
             <ul class="flex">
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
+                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
                     <a href="#">Laravel</a>
                 </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
+                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
                     <a href="#">API</a>
                 </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
+                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
                     <a href="#">Backend</a>
                 </li>
                 <li
@@ -35,5 +31,5 @@
             <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
             </div>
         </div>
-    </div>
+    </x-card>
 </div>
