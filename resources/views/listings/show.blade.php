cdcdb819e6d6200a70dc6dc687fd806842b09f5a
Edmond GAKS <edmondgaks@gmail.com>
2022-08-09 22:58:45 +0200

    adding the edit button

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -43,6 +43,11 @@
         </div>
     </div>
 </x-card>
+<x-card class="mt-4 p-2 flex space-x-6">
+    <a href="/listings/{{$listing->id}}/edit">
+        <i class="fa-solid fa-pencil"></i>Edit
+    </a>
+</x-card>
 </div>
 
 </x-layout>
