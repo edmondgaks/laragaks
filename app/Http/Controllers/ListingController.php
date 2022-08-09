7f3f1f1fc2d4cf8e424d85f0c0faca2e2af029f3
Edmond GAKS <edmondgaks@gmail.com>
2022-08-09 10:43:17 +0200

    using controllers in routes

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Listing;
+
 
 class ListingController extends Controller
 {
-    
+    // Show all listings
+    public function index() {
+        return view('listings', [
+            'listings' => Listing::all()
+        ]);
+    }
+    // Show single listing
+    public function show(Listing $listing) {
+        return view('Listing', [
+            'listing' => $listing
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -13,15 +15,6 @@ use App\Models\Listing;
 |
 */
 
-Route::get('/', function () {
-    return view('listings', [
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
  
-Route::get('/listings/{listing}', function(Listing $listing) {
-    return view('Listing', [
-        'listing' => $listing
-    ]);
-
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
