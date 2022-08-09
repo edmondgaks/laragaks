1043a6d766b4dc78d3a6a4ca2efad0a53cade78c
Edmond GAKS <edmondgaks@gmail.com>
2022-08-09 23:11:16 +0200

    moving error msgs inside and indentation

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -15,45 +15,45 @@
             <div class="mb-6">
                 <label for="company" class="inline-block text-lg mb-2">Company Name</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" value="{{old('company')}}" />
+                @error('company')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('company')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             <div class="mb-6">
                 <label for="title" class="inline-block text-lg mb-2">Job Title</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
                 placeholder="Example: Senior Laravel Developer" value="{{old('title')}}" />
+                @error('title')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('title')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <label for="location" class="inline-block text-lg mb-2">Job Location</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
                 placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}" />
+                @error('location')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('location')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Contact Email</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}" />
+                @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('email')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
 
             <div class="mb-6">
                 <label for="website" class="inline-block text-lg mb-2">
                     Website/Application URL
                 </label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website" value="{{old('website')}}" />
+                @error('website')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('website')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <label for="tags" class="inline-block text-lg mb-2">
@@ -61,20 +61,20 @@
                 </label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
                 placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}" />
+                @error('tags')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div> 
-            @error('tags')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <label for="logo" class="inline-block text-lg mb-2">
                     Company Logo
                 </label>
                 <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo" />
+                @error('logo')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('logo')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">
@@ -82,10 +82,10 @@
                 </label>
                 <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
                 placeholder="Include tasks, requirements, salary, etc">{{old('description')}}</textarea>
+                @error('description')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('description')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -15,66 +15,71 @@
             <div class="mb-6">
                 <label for="company" class="inline-block text-lg mb-2">Company Name</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" value="{{$listing->company}}" />
+                @error('company')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('company')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             <div class="mb-6">
                 <label for="title" class="inline-block text-lg mb-2">Job Title</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
                 placeholder="Example: Senior Laravel Developer" value="{{$listing->title}}" />
+                @error('title')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('title')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <label for="location" class="inline-block text-lg mb-2">Job Location</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
                 placeholder="Example: Remote, Boston MA, etc" value="{{$listing->location}}" />
+                @error('location')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('location')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Contact Email</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{$listing->email}}" />
+                @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('email')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
 
             <div class="mb-6">
                 <label for="website" class="inline-block text-lg mb-2">
                     Website/Application URL
                 </label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website" value="{{$listing->website}}" />
+                @error('website')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
+                
             </div>
-            @error('website')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
-            
             <div class="mb-6">
                 <label for="tags" class="inline-block text-lg mb-2">
                     Tags (Comma Separated)
                 </label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
                 placeholder="Example: Laravel, Backend, Postgres, etc" value="{{$listing->tags}}" />
+                @error('tags')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div> 
-            @error('tags')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <label for="logo" class="inline-block text-lg mb-2">
                     Company Logo
                 </label>
                 <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo" />
-            </div>
-            @error('logo')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
+                <img
+            class="w-48 mr-6 mb-6"
+            src="{{$listing->logo ? asset('storage/'. $listing->logo) : asset('/images/no-image.png')}}"
+            alt=""
+        />
+        @error('logo')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+        @enderror
+            </div> 
             
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">
@@ -82,10 +87,10 @@
                 </label>
                 <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
                 placeholder="Include tasks, requirements, salary, etc">{{$listing->description}}</textarea>
+                @error('description')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                @enderror
             </div>
-            @error('description')
-                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-            @enderror
             
             <div class="mb-6">
                 <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
