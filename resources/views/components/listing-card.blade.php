62028b0a85655a7b704a476dade41015d72b2a04
Edmond GAKS <edmondgaks@gmail.com>
2022-08-09 10:19:58 +0200

    getting tags from the database and using them by exploding

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -12,22 +12,7 @@
                 <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
-            <ul class="flex">
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Laravel</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">API</a>
-                </li>
-                <li class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                    <a href="#">Backend</a>
-                </li>
-                <li
-                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
-                >
-                    <a href="#">Vue</a>
-                </li>
-            </ul>
+            <x-listing-tags :tagsCsv="$listing->tags" />
             <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
             </div>
         </div>
